b85afb0f493c10264fad9392a8b7a29c32253ddb
Jon Leopard <jonlprd@gmail.com>
2018-02-08 17:00:56 +0100

    Articles finished. Currently looking into a TypeError on dummy-data.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Players from './Players';
 import Teams from './Teams';
 import Navbar from './Navbar';
 import TeamPage from './TeamPage';
+import Articles from './Articles';
+
 
 class App extends Component {
   render() {
@@ -17,6 +19,7 @@ class App extends Component {
             <Route path='/players' component={Players} />
             <Route path='/teams' component={Teams} />
             <Route path='/:teamId' exact component={TeamPage} />
+            <Route path='/:teamId/articles' component={Articles} />
             <Route render={() => <h1 className='text-center'>404 - BUMMER!</h1>} />'}
           </Switch>
         </div>
--- src/components/TeamPage.js
+++ src/components/TeamPage.js
@@ -3,7 +3,9 @@ import { Link, Redirect } from 'react-router-dom';
 import { getTeamsArticles, getTeamNames } from '../api';
 import TeamLogo from './TeamLogo';
 import Team from './Team';
+import Loading from './Loading';
 import slug from 'slug';
+
 
 export default class TeamPage extends Component {
   state = {
@@ -40,7 +42,7 @@ export default class TeamPage extends Component {
       <div>
         <Team id={teamId}>
           {(team) => team === null
-            ? <h1>LOADING</h1>
+            ? <Loading />
             : <div className='panel'>
                 <TeamLogo id={teamId} />
                 <h1 className='medium-header'>{team.name}</h1>
